a677dd9bd69cf2e82df07501f017bc48d6459eed
Meesie2109 <meesrootjes@icloud.com>
2021-12-01 17:01:10 +0100

    Added comments to the code

--- Day1/dayOne.js
+++ Day1/dayOne.js
@@ -5,6 +5,8 @@ const lines = fs
   .filter((x) => x)
   .map((x) => parseInt(x));
 
+//Advant of code 2021 day 1 question 1
+//Function check of number 2 is bigger than number 1
 const isNumberBiggerThamPrevious = () => {
   let number = 0;
   for (let i = 0; i < lines.length; i++) {
@@ -15,6 +17,9 @@ const isNumberBiggerThamPrevious = () => {
   return number;
 };
 
+//Advant of code 2021 day 1 question 2
+//Function checks of number 2,3,4 from the data file are larger
+//than number 1, 2, 3
 const threeNumbersBiggerThanPrevious = () => {
   let number = 0;
   for (let i = 0; i < lines.length; i++) {
