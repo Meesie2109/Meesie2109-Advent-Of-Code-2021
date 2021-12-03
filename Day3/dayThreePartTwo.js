b67a50e98037d8d88b7055df7dfa3f2b4aa73f7f
Meesie2109 <meesrootjes@icloud.com>
2021-12-03 16:10:50 +0100

    Made day 3 part 2 more effecient

--- Day3/dayThreePartTwo.js
+++ Day3/dayThreePartTwo.js
@@ -9,24 +9,22 @@ const oxygenGeneratorRateBits = [];
 const co2ScrubberRateBits = [];
 let digit = 0;
 
-const mostCommonValue = (array, digit) => {
+const commonValue = (array, digit, i) => {
   const ones = array.reduce((ones, number) => ones + +number[digit], 0);
-
-  return ones !== array.length / 2
-    ? ones > Math.floor(array.length / 2)
-      ? "1"
-      : "0"
-    : "1";
-};
-
-const leastCommonValue = (array, digit) => {
-  const ones = array.reduce((ones, number) => ones + +number[digit], 0);
-
-  return ones !== array.length / 2
-    ? ones < array.length / 2
-      ? "1"
-      : "0"
-    : "0";
+  if (i === 1) {
+    return ones !== array.length / 2
+      ? ones > Math.floor(array.length / 2)
+        ? "1"
+        : "0"
+      : "1";
+  }
+  if (i === 2) {
+    return ones !== array.length / 2
+      ? ones < array.length / 2
+        ? "1"
+        : "0"
+      : "0";
+  }
 };
 
 const parseInteger = (input) => {
@@ -38,31 +36,28 @@ const lifeSupport = () => {
   while (digit < numbers[0].length) {
     const oxygenGeneratorRateMask = oxygenGeneratorRateBits.join("");
     const co2ScrubberRateMask = co2ScrubberRateBits.join("");
-
-    const NumbersFiltered = (span, value) => {
+    const NumbersFiltered = (span, value, i) => {
       const filteredNumbers = numbers.filter(
         (element) => element.slice(0, digit).join("") === span
       );
       const number =
         filteredNumbers.length === 1
           ? filteredNumbers[0][digit]
-          : value(filteredNumbers, digit);
+          : value(filteredNumbers, digit, i);
       return number;
     };
 
     oxygenGeneratorRateBits.push(
-      NumbersFiltered(oxygenGeneratorRateMask, mostCommonValue)
+      NumbersFiltered(oxygenGeneratorRateMask, commonValue, 1)
     );
     co2ScrubberRateBits.push(
-      NumbersFiltered(co2ScrubberRateMask, leastCommonValue)
+      NumbersFiltered(co2ScrubberRateMask, commonValue, 2)
     );
 
     digit++;
   }
-
   const lifeSupportRating =
     parseInteger(oxygenGeneratorRateBits) * parseInteger(co2ScrubberRateBits);
-
   return lifeSupportRating;
 };
 
